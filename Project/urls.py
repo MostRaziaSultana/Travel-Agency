8572669b6c4959c8167aab19783866a544e7c7e3
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-10-09 22:36:50 +0600

    environment

---
+++ Project/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from Home.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
