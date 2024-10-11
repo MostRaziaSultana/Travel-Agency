0feccc70e228acc8da57f26a28c774d0e820da21
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-10-11 19:59:07 +0600

    vendor create

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 # Register your models here.
+
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_vendor',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('is_vendor',)}),  # Add is_vendor field to add form
+    )
+    list_display = UserAdmin.list_display + ('is_vendor',)  # Optional: add to list view
+    list_filter = UserAdmin.list_filter + ('is_vendor',)  # Optional: add filter
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Package, Booking
 
-# Register your models here.
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('package_code', 'package_name', 'destination', 'price', 'available_seats', 'status', 'creator', 'created_at', 'updated_at')
+    search_fields = ('package_code', 'package_name', 'destination')
+    list_filter = ('status', 'creator', 'start_date', 'end_date')
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.has_perm('Home.can_crud_package')
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.has_perm('Home.can_crud_package')
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.has_perm('Home.can_crud_package')
+
+    def has_add_permission(self, request):
+        return request.user.has_perm('Home.can_crud_package')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs  # Allow superusers to see all packages
+        return qs.filter(creator=request.user)
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('customer_id', 'package_id', 'booking_date', 'status', 'payment_status', 'travel_status', 'price_at_booking', 'created_at', 'updated_at')
+    search_fields = ('customer_id__username', 'package_id__package_name')
+    list_filter = ('status', 'payment_status', 'travel_status', 'booking_date')
+
+
+admin.site.register(Package, PackageAdmin)
+admin.site.register(Booking, BookingAdmin)
