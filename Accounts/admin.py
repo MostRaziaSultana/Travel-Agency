de119f739ac865e8ed993087543faddd52a75329
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-11-14 18:08:36 +0600

    update

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser,Profile
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import CustomUser,Profile
 # Register your models here.
 
 
@@ -19,3 +19,41 @@ from .models import CustomUser,Profile
 # # admin.site.register(CustomUser, CustomUserAdmin)
 #
 # admin.site.register(Profile)
+
+
+
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
+
+class CustomUserAdmin(UserAdmin):
+    # Add 'Update' and 'Delete' fields in the user list display
+    list_display = UserAdmin.list_display + ('update_link', 'delete_link')
+    list_per_page = 20
+
+    def update_link(self, obj):
+        # Create an update link for each user
+        update_url = reverse('admin:auth_user_change', args=[obj.pk])
+        return format_html(
+            '<a class="button" href="{}" style="color: white; background-color: green; padding: 5px 10px; border-radius: 5px; text-decoration: none;">Update</a>',
+            update_url
+        )
+
+    update_link.short_description = 'Update'
+
+    def delete_link(self, obj):
+        # Create a delete link for each user
+        delete_url = reverse('admin:auth_user_delete', args=[obj.pk])
+        return format_html(
+            '<a class="button" href="{}" style="color: white; background-color: red; padding: 5px 10px; border-radius: 5px; text-decoration: none;">Delete</a>',
+            delete_url
+        )
+
+    delete_link.short_description = 'Delete'
+
+# Unregister the existing User admin and register the customized one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
