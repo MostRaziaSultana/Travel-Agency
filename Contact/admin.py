9b9aedbd353067cdbf28d160a5f6391db90e43af
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-12-03 22:08:43 +0600

    update

--- Contact/admin.py
+++ Contact/admin.py
@@ -29,21 +29,35 @@ class ContactUsAdmin(admin.ModelAdmin):
 admin.site.register(ContactUs, ContactUsAdmin)
 
 class UserMessageAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'email', 'mobile', 'created_at', 'seen', 'view_link', 'delete_link')
+    list_display = ('id_with_tag','name', 'email', 'mobile', 'created_at', 'seen', 'view_link', 'delete_link')
     search_fields = ('name', 'email', 'mobile')
     list_per_page = 20
+
+    def id_with_tag(self, obj):
+        """
+        Display the ID with a "New" tag if the UserMessage is unseen.
+        """
+        if not obj.seen:
+            return format_html(
+                '{} <span style="color: white; background-color: #385a7f; padding: 2px 5px; border-radius: 3px; font-size: 0.9em;">New</span>',
+                obj.id
+            )
+        return obj.id
+
+    id_with_tag.short_description = "ID"
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         obj = self.get_object(request, object_id)
         if obj and not obj.seen:
             obj.seen = True
-            obj.save()  # Mark as seen
+            obj.save()
         return super(UserMessageAdmin, self).change_view(request, object_id, form_url, extra_context)
+
 
     def view_link(self, obj):
         view_url = reverse('admin:Contact_usermessage_change', args=[obj.id])
         return format_html(
-            '<a class="button" href="{}" style="color: white; background-color: blue; padding: 5px 10px; border-radius: 5px; text-decoration: none;">View</a>',
+            '<a class="button" href="{}" style="color: white; background-color: #385a7f; padding: 5px 10px; border-radius: 5px; text-decoration: none;">View</a>',
             view_url
         )
 
@@ -67,4 +81,6 @@ class UserMessageAdmin(admin.ModelAdmin):
     delete_link.short_description = 'Delete'
 
 admin.site.register(UserMessage, UserMessageAdmin)
+
+
 
